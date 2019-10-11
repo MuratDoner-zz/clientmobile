bbc2274098a3ff7aa1f484c868d9e81a32c43847
murat.doner <murat.doner@atg.se>
2019-10-11 18:33:01 +0200

    The first Rest trial.

--- app/src/main/java/sample/SampleAndroid.kt
+++ app/src/main/java/sample/SampleAndroid.kt
@@ -1,10 +1,13 @@
 package sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import kotlinx.coroutines.runBlocking
 
 actual class Sample {
     actual fun checkMe() = 44
@@ -27,7 +30,27 @@ class MainActivity : AppCompatActivity() {
         // set on-click listener
         button.setOnClickListener {
             // your code to perform when the user clicks on the button
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity, "Rest Service trial:", Toast.LENGTH_SHORT).show()
+            val client = HttpClient()
+            getUsers(client)
+
+
         }
     }
+    private fun getUsers(client: HttpClient) {
+        val url= "https://reqres.in/api/users"
+
+        // Block execution till request is completed
+        runBlocking {
+
+
+            client.get<String>(url)
+        }.let {users ->
+
+            Toast.makeText(this@MainActivity, "USERS JSON:" + users, Toast.LENGTH_SHORT).show()
+
+            print(users)
+        }
+    }
+
 }
